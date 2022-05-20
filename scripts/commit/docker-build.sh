9b5b46ae989b26b133c1df6944afd0357c34d067
christi4n <christian@mydevfactory.net>
2022-05-20 10:53:49 +0200

    [TASK] Optimize

--- scripts/commit/docker-build.sh
+++ scripts/commit/docker-build.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+while getopts d:i: flag
+do
+    case "${flag}" in
+        d) dockerhub_username=${OPTARG};;
+        i) image_name=${OPTARG};;
+    esac
+done
+
 echo "=========== Building Docker Image: Start =============="
 
-ls -al
-
-docker build -t amarissuisse/gocd-agent-java8:$GO_PIPELINE_LABEL . -f Docker/Dockerfile.agent
+docker build -t $dockerhub_username/$image_name:$GO_PIPELINE_LABEL . -f Docker/Dockerfile.agent
 
 echo "=========== Building Docker Image: Done  =============="
