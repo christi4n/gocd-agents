8ccb9ceb2d232a68c161f153a1f02dde15ffb9a1
christi4n <christian@mydevfactory.net>
2022-05-20 09:54:25 +0200

    [BUGFIX] Change path to Dockerfile

--- scripts/commit/docker-build.sh
+++ scripts/commit/docker-build.sh
@@ -2,6 +2,8 @@
 
 echo "=========== Building Docker Image: Start =============="
 
-docker build -t amarissuisse/gocd-agent-java8:$GO_PIPELINE_LABEL . -f ../../Docker/Dockerfile.agent
+ls -al
+
+docker build -t amarissuisse/gocd-agent-java8:$GO_PIPELINE_LABEL . -f Docker/Dockerfile.agent
 
 echo "=========== Building Docker Image: Done  =============="
