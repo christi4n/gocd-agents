0c7125bbefbd6e09c9b7be82085d8fd9c3f3045e
christi4n <christian@mydevfactory.net>
2022-05-20 09:48:49 +0200

    [TASK] Build new image command line

--- scripts/commit/docker-build.sh
+++ scripts/commit/docker-build.sh
@@ -2,6 +2,6 @@
 
 echo "=========== Building Docker Image: Start =============="
 
-sleep 4
+docker build -t amarissuisse/gocd-agent-java8:$GO_PIPELINE_LABEL . -f ../../Docker/Dockerfile.agent
 
 echo "=========== Building Docker Image: Done  =============="
