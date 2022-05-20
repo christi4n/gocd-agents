6506aa0a92e8e31123a16771936e74de85a86bba
christi4n <christian@mydevfactory.net>
2022-05-20 15:43:05 +0200

    [TASK] Fix env vars

--- scripts/commit/docker-build.sh
+++ scripts/commit/docker-build.sh
@@ -1,15 +1,7 @@
 #!/bin/sh
 
-while getopts d:i: flag
-do
-    case "${flag}" in
-        d) dockerhub_username=${OPTARG};;
-        i) image_name=${OPTARG};;
-    esac
-done
-
 echo "=========== Building Docker Image: Start =============="
 
-docker build -t $dockerhub_username/$image_name:$GO_PIPELINE_LABEL . -f Docker/Dockerfile.agent
+docker build -t $DOCKERHUB_USERNAME/$IMAGE_NAME:$GO_PIPELINE_LABEL . -f Docker/Dockerfile.agent
 
 echo "=========== Building Docker Image: Done  =============="
